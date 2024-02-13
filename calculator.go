5daa0bda5118ff5d1ac8007e030f3490d6596101
John Amicangelo <jamicangelo@paxos.com>
2024-02-13 15:08:17 -0500

    added divide

--- calculator.go
+++ calculator.go
@@ -10,7 +10,12 @@ func Subtract(x, y int) int {
 	return x - y
 }
 
-// Multiply subtracts y from x
+// Multiply multiple x and y
 func Multiply(x, y int) int {
 	return x * y
+}
+
+// Divide divide y from x
+func Divide(x, y int) int {
+	return x / y
 }
--- calculator_test.go
+++ calculator_test.go
@@ -19,3 +19,9 @@ func TestMultiply(t *testing.T) {
 		t.Errorf("Multiply function produced wrong result. expected: %d, got: %d", want, got)
 	}
 }
+
+func TestDivide(t *testing.T) {
+	if got, want := Divide(4, 2), 2; got != want {
+		t.Errorf("Divide function produced wrong result. expected: %d, got: %d", want, got)
+	}
+}
